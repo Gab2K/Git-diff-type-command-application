cc69621c7894ad054b01f9b3ccf36469b39ed6bd
Gab2K <60928508+Gab2K@users.noreply.github.com>
2022-08-30 21:13:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Git_diff
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var file1 = new List<string>(); //File 1 is going to be loaded here/
+            var file2 = new List<string>(); //File 2 is going to be loaded here
+
+            var takenWordsList = new List<string>(); //File with all the subtracted words
+            var addedWordsList = new List<string>(); //File with all the added words
+
+
+            int numberOfFiles = 0; //To get number of files that are available
+
+
+            //creates a new string of files 
+            string[] files =
+            {   "1.txt", "2.txt", "3.txt", "4.txt", "5.txt",
+                "GitRepositories_1a.txt", "GitRepositories_1b.txt",
+                "GitRepositories_2a.txt", "GitRepositories_2b.txt",
+                "GitRepositories_3a.txt", "GitRepositories_3b.txt" };
+
+            //loops through the string of files
+            for (int i = 0; i < files.Length; i++)
+            {
+                numberOfFiles++;
+                Console.Write($"{i + 1}. ");
+                Console.Write(files[i]);
+                Console.WriteLine();
+            }
+
+
+            bool correctInput = false; //Checks if input is correct
+            int First; //First file input
+            int Second; //Second file input
+
+
+            // First input
+            while (correctInput == false) //While user doesn't input in correct format
+            {
+                try
+                {
+                    Console.Write("\nEnter the first file (number): ");
+                    First = Int32.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException) //If input is not a number
+                {
+                    Console.WriteLine($"\nEnter a number between 1 and {numberOfFiles}");
+                    continue; //Go back to the beginning of a loop for the first file
+                }
+
+                //If input is bigger than 0 or smaller or equal to number of files
+                if (First > 0 && First <= numberOfFiles)
+                {
+                    First--;
+                    differenceCheck.loadFiles(files[First], file1); //Passes the file name to the function
+                    correctInput = true; //Breaks the loop
+                }
+                else
+                {
+                    Console.WriteLine($"\nEnter a number between 1 and {numberOfFiles}");
+                    continue; //Goes to the beginning of the loop for first file
+                }
+
+            }
+
+            correctInput = false; //Resets correct input so it can be used for the second file
+
+
+            // Second file
+            while (correctInput == false) //While user doesn't input in correct format
+            {
+                try
+                {
+                    Console.Write("\nEnter the second file (number): ");
+                    Second = Int32.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException) //If input is not a number
+                {
+                    Console.WriteLine($"\nEnter a number between 1 and {numberOfFiles}");
+                    continue; //Go back to the beginning of a loop for second file
+                }
+
+                //If input is bigger than 0 or smaller or equal to number of files
+                if (Second > 0 && Second <= numberOfFiles)
+                {
+                    Second--;
+                    differenceCheck.loadFiles(files[Second], file2); //Passes the file name to the function
+                    differenceCheck.compareFiles(file1, file2, takenWordsList, addedWordsList); //Load the function that compares the 2 files
+                    correctInput = true; //Breaks the loop
+                }
+                else
+                {
+                    Console.WriteLine($"\nEnter a number between 1 and {numberOfFiles}");
+                    continue; //Goes to the beginning of the loop for second file
+                }
+
+            }
+
+            Console.Clear();
+
+            //Loops through first file
+            Console.WriteLine(">>File1<<\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            foreach (var item in file1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ResetColor();
+
+            //Loops through second file
+            Console.WriteLine("\n>>File2<<\n");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            foreach (var item in file2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ResetColor();
+            Console.WriteLine("\n\nPress enter for results\n");
+            Console.ReadLine();
+
+            if (takenWordsList.Count == 0 && addedWordsList.Count == 0) //If both lists are empty it means that both files are the same
+            {
+                Console.WriteLine("Files are the same");
+            }
+
+            else
+            {
+                //Outputs all the words that have been removed in red colour
+                Console.ForegroundColor = ConsoleColor.Red;
+                foreach (var item in takenWordsList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.ResetColor();
+
+                //Outputs all the words that have been added in green colour
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach (var item in addedWordsList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.ResetColor();
+
+
+                //Create a log 
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", DateTime.Now.ToString()); //Append date to log file
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "\n"); //New line
+
+                foreach (var item in addedWordsList)
+                {
+                    File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", item.ToString());
+                    File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "\n");
+                }
+
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "\n"); //New line
+                foreach (var item in takenWordsList)
+                {
+                    File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", item.ToString());
+                    File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "\n"); //New line
+                }
+
+                Console.WriteLine($"\nLog file has been created at \n{AppDomain.CurrentDomain.BaseDirectory}");
+            }
+            
+        }
+    }
+}
+
+
+
+
+    
+
+
---
+++ differenceCheck.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Git_diff
+{
+	public class differenceCheck
+	{
+
+		//Arguments: Name of a file, list where text from file is going to be added
+		public static void loadFiles(string fileName, List<string> fileInList)
+		
+		{
+			int lineNumber = 1;
+			// Read the file line by line and append to a list
+			using StreamReader reader = new StreamReader(fileName);
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				fileInList.Add($"[Line: {lineNumber}]");
+				fileInList.Add(line); //Adds to a list
+				lineNumber++;
+			}
+
+
+		}
+
+
+		//Arguments: First split list, Second split list, List where everything is going to be added
+		public static void compareFiles(List<string> file1, List<string> file2, List<string> taken, List<string> added)
+		
+		{
+			//Gets the shorter list for the loop
+			int shorter;
+			if (file1.Count < file2.Count)
+			{
+				shorter = file1.Count;
+			}
+			else
+			{
+				shorter = file2.Count;
+			}
+
+
+			for (int i = 0; i < shorter; i++)
+			{
+				//Spliting each line into words
+
+				char[] splitCharacters = {(char)32, (char)39}; //space and apostrophe
+				string[] file1Split = file1[i].Split(splitCharacters);
+				string[] file2Split = file2[i].Split(splitCharacters);
+
+				
+				if (file1Split.Length > file2Split.Length) //If file 1 is longer then words have been removed
+				{
+					for (int j = 0; j < file2Split.Length; j++)
+					{
+						if (file1Split[j] != file2Split[j])
+						{
+							taken.Add($"'{file1Split[j]}' removed at line {(i+1)/2} index {j+1} " +
+								$"and replaced with '{file2Split[j]}'");
+						}
+					}
+
+				}
+				else //Otherwise it is bigger or the same, so words have been added
+				{
+					for (int k = 0; k < file1Split.Length; k++)
+					{
+						if (file1Split[k] != file2Split[k])
+						{
+							added.Add($"'{file2Split[k]}' added at line {(i+1)/2} index {k + 1}, " +
+								$"replacing '{file1Split[k]}'");
+						}
+					}
+
+				}
+			}
+
+
+
+		}
+	}
+	}
+
+
+
+
+	
+
+
+
